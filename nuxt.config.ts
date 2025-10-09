9421b9154063f61c47ba31f311aa087b525321a2
Alix <alix@krlx.fr>
2025-10-09 11:42:29 +0200

    update nitro preset to cloudflare-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
   },
   // --- CONFIGURATION DE SERVEUR ---
   nitro: {
-    preset: 'static',
+    preset: 'cloudflare-pages',
     prerender: {
       routes: ['/', 'login', 'pwa'],
     },
